95dee6f7b367b4bb93f9a13327c70139f3886d86
Stella Delonge <s.delonge@tum.de>
2021-01-11 13:28:34 +0100

    Added: Spine logic: When not registered, adjust position via NearInteractionGrabbable and Bounding Box When registered, currently not implemented Switch between modes when sight of markers is gained/lost (and maintained or not seen for 2 seconds to avoid frequent switches for small errors) Simulate marker sight gained/lost with the B key

---
+++ MedARProject/Assets/Scripts/Base/HologramUI.cs
@@ -0,0 +1,71 @@
+﻿//Stella
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Microsoft.MixedReality.Toolkit.UI;
+
+
+/// <summary>
+/// Behaviour to "billboard" ui in front of hologram (copied from AppBar)
+/// </summary>
+public class HologramUI : MonoBehaviour
+{
+    private const float moveSpeed = 5;
+
+    [SerializeField]
+    private BoxCollider _collider;
+
+    [SerializeField]
+    private float _offset = 0f;
+
+    private BoundingBoxHelper helper = new BoundingBoxHelper();
+    private List<Vector3> boundsPoints = new List<Vector3>();
+
+    private void OnEnable()
+    {
+        FollowTargetObject(false);
+    }
+
+    private void Update()
+    {
+        FollowTargetObject(true);
+    }
+
+    private void FollowTargetObject(bool smooth)
+    {
+        if (_collider == null)
+            return;
+
+        // Calculate the best follow position
+        Vector3 finalPosition = Vector3.zero;
+        Vector3 headPosition = CameraCache.Main.transform.position;
+        boundsPoints.Clear();
+
+        helper.UpdateNonAABoundsCornerPositions(_collider, boundsPoints);
+        int followingFaceIndex = helper.GetIndexOfForwardFace(headPosition);
+        Vector3 faceNormal = helper.GetFaceNormal(followingFaceIndex);
+
+        // Finalize the new position
+        finalPosition = helper.GetFaceBottomCentroid(followingFaceIndex) + (faceNormal * _offset);
+
+        // Follow our bounding box
+        transform.position = smooth ? Vector3.Lerp(transform.position, finalPosition, Time.deltaTime * moveSpeed) : finalPosition;
+
+        // Rotate on the y axis
+        Vector3 direction = (_collider.bounds.center - finalPosition).normalized;
+        if (direction != Vector3.zero)
+        {
+            Vector3 eulerAngles = Quaternion.LookRotation(direction, Vector3.up).eulerAngles;
+            eulerAngles.x = 0f;
+            eulerAngles.z = 0f;
+            transform.eulerAngles = eulerAngles;
+        }
+        else
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+    }
+
+}
---
+++ MedARProject/Assets/Scripts/Spine/Spine.cs
@@ -0,0 +1,134 @@
+﻿//Stella
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.UI;
+
+/// <summary>
+/// Responsible for Spine positioning behaviour modes
+/// Switch between registered and non-registered
+/// Non-registered uses nearInteractionGrabbable + bounding box for adjustments
+/// 
+/// Currently: 
+/// if non-registered and markers are in sight for more than x seconds -> registered
+/// if registered and markers are out of sight for more than x seconds -> non-registered
+/// 
+/// (Test using B key: down = marker sight, up = no marker sight)
+/// </summary>
+public class Spine : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _model;
+
+    [SerializeField]
+    private GameObject _nonRegisteredLogicObject;
+    [SerializeField]
+    private Transform _nonRegisteredLogicTarget;
+
+    [SerializeField]
+    private GameObject _loadingBarObject;
+    [SerializeField]
+    private ProgressIndicatorLoadingBar _loadingBar;
+
+
+    private bool _registered;
+
+    private void Update()
+    {
+        //Since there are two ways to position the spine, we move logic objects
+        //Only the logic object for the current phase is active
+        //Because of that, we need to copy transform values from the respective logic objects
+
+        if (!_registered)
+        {
+            _model.transform.position = _nonRegisteredLogicTarget.transform.position;
+            _model.transform.rotation = _nonRegisteredLogicTarget.transform.rotation;
+            _model.transform.localScale = _nonRegisteredLogicTarget.transform.localScale;
+        }
+        else
+        { 
+            //TODO: set transform to registered values
+        }
+
+        //DUMMY for testing
+        if (Input.GetKeyDown(KeyCode.B))
+            OnMarkerSightGained();
+        if (Input.GetKeyUp(KeyCode.B))
+            OnMarkerSightLost();
+    }
+
+    private void OnMarkerSightGained()
+    {
+        Debug.Log("Gained sight of Spine markers!");
+        stopSwitchPhase();
+        if (!_registered)
+            _coroSwitchPhase = StartCoroutine(switchPhase(true));
+    }
+    private void OnMarkerSightLost()
+    {
+        Debug.Log("Lost sight of Spine markers!");
+        stopSwitchPhase();
+        if (_registered)
+            _coroSwitchPhase = StartCoroutine(switchPhase(false));
+    }
+
+    private Coroutine _coroSwitchPhase;
+    private System.Threading.Tasks.Task _taskCloseLoadingBar;
+
+    private IEnumerator switchPhase(bool registered)
+    {
+        //Wait until loading bar has closed if closed by cancellation of switch
+        while (_taskCloseLoadingBar != null && !_taskCloseLoadingBar.IsCompleted)
+            yield return null;
+        _taskCloseLoadingBar = null;
+
+        //reset and open loading bar
+        _loadingBar.Progress = 0f;
+        System.Threading.Tasks.Task taskOpen = _loadingBar.OpenAsync();
+        while (!taskOpen.IsCompleted)
+            yield return null;
+        _loadingBarObject.SetActive(true);
+
+        _loadingBar.Message = registered ? "register..." : "marker sight lost...";
+
+        //Update Progress
+        float interval = 0.1f;
+        float max = 2f;
+        for (float f = 0; f < max; f += interval)
+        {
+            yield return new WaitForSeconds(interval);
+            _loadingBar.Progress = f / max;
+        }
+
+        //Close Loading Bar
+        System.Threading.Tasks.Task taskClose = _loadingBar.CloseAsync();
+        while (!taskClose.IsCompleted)
+            yield return null;
+        _loadingBarObject.SetActive(false);
+        _registered = registered;
+
+        if (!_registered)
+        {
+            //switching to manual positioning - update bounding box to be at last transform values
+            _nonRegisteredLogicTarget.transform.position = _model.transform.position;
+            _nonRegisteredLogicTarget.transform.rotation = _model.transform.rotation;
+            _nonRegisteredLogicTarget.transform.localScale = _model.transform.localScale;
+        }
+
+        //Set respective logic objects active/inactive
+        _nonRegisteredLogicObject.SetActive(!_registered);
+    }
+
+    private void stopSwitchPhase()
+    {
+        //Stops switching phase process if sight gained/lost again during switching process
+        if (_coroSwitchPhase != null)
+            StopCoroutine(_coroSwitchPhase);
+
+        _taskCloseLoadingBar = _loadingBar.CloseAsync();
+        _loadingBarObject.SetActive(false);
+    }
+}
+
+
