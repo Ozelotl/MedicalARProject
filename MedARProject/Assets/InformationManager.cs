69e53465df1d19ff7b47676c99642e03ae48cfc9
LarsHeckler <lars.heckler@tum.de>
2021-02-01 12:09:45 +0100

    added prefab for showing information during the procedure; dummy placed in guide_placement scene for testing

---
+++ MedARProject/Assets/InformationManager.cs
@@ -0,0 +1,55 @@
+﻿//Lars
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+/// <summary>
+/// show/hide information on UI
+/// </summary>
+public class InformationManager : SingletonMonoMortal<InformationManager>
+{
+
+    public enum Info
+    {
+        Show,
+        Hide
+    }
+
+
+    #region fields
+    [Header("Scene References")]
+    [SerializeField]
+    private GameObject OpenButton;
+    [SerializeField]
+    private GameObject CloseButton;
+    [SerializeField]
+    private GameObject Content;
+
+    #endregion
+
+
+    private void Start()
+    {
+        showInfo();
+    }
+
+    public void showInfo()
+    {
+        OpenButton.SetActive(false); // should be set inactive by default
+        CloseButton.SetActive(true); // should be set active by default
+        Content.SetActive(true); // should be set active by default
+    }
+
+
+
+    public void hideInfo()
+    {
+        OpenButton.SetActive(true);
+        CloseButton.SetActive(false);
+        Content.SetActive(false);
+    }
+
+}
