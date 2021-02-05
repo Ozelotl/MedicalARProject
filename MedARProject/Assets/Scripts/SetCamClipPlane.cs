1cf0a0771e07d88497549ac91e3c5705346a85cb
Stella Delonge <s.delonge@tum.de>
2021-02-05 22:15:13 +0100

    Set ar cam clip plane and spine registration in scene

---
+++ MedARProject/Assets/Scripts/SetCamClipPlane.cs
@@ -0,0 +1,22 @@
+﻿//Stella
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Camera automatically gets set to 0.3 clip plane
+/// but we need to be able to look at entry point at close ranges
+/// </summary>
+public class SetCamClipPlane : MonoBehaviour
+{
+    [SerializeField]
+    private float _nearClipPlane;
+
+    void Start()
+    {
+        Camera cam = gameObject.GetComponent<Camera>();
+        if (cam != null)
+            cam.nearClipPlane = _nearClipPlane;
+    }
+}
